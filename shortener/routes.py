a490870ee4ea2bec864d230853837e893d17b6eb
Sergey Demenok <sergey.demenok@gmail.com>
2019-03-15 10:54:18 +0300

    Add regex for redirect

--- shortener/routes.py
+++ shortener/routes.py
@@ -3,4 +3,4 @@
 
 def setup_routes(app, handler):
     app.router.add_post('/generate_short_link', handler.generate)
-    app.router.add_get('/{short_id}', handler.redirect)
+    app.router.add_get('/{short_id:[a-zA-Z0-9]+}', handler.redirect)
